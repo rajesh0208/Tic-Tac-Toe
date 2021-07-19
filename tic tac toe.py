eee95ca2aa7122fcda5613f92427b9564045c11a
rajesh0208 <68410372+rajesh0208@users.noreply.github.com>
2021-07-19 20:39:02 +0530

    Add files via upload

---
+++ tic tac toe.py
@@ -0,0 +1,86 @@
+theBoard = {'7': ' ' , '8': ' ' , '9': ' ' ,
+            '4': ' ' , '5': ' ' , '6': ' ' ,
+            '1': ' ' , '2': ' ' , '3': ' ' }
+board_keys=[]
+for key in theBoard:
+    board_keys.append(key)
+def printBoard(board):
+    print(board['7'] + '|' + board['8'] + '|' + board['9'])
+    print('-+-+-')
+    print(board['4'] + '|' + board['5'] + '|' + board['6'])
+    print('-+-+-')
+    print(board['1'] + '|' + board['2'] + '|' + board['3'])
+def game():
+    t='X'
+    c=0
+    for i in range(9):
+        printBoard(theBoard)
+        print("It's your turn,"+ t +".move to which place?")
+        move=input()
+        if(theBoard[move]==' '):
+            theBoard[move]=t
+            c+=1
+        else:
+            print("That place is already filled.\nmove to which place?")
+            continue
+        if(c>=5):
+            if(theBoard['7']==theBoard['8']==theBoard['9']!=' '):
+                printBoard(theBoard)
+                print("\nGame Over.\n")
+                print("****"+t+"won.****")
+                break
+            elif(theBoard['4']==theBoard['5']==theBoard['6']!=' '):
+                printBoard(theBoard)
+                print("\nGame Over.\n")
+                print("****"+t+"won.****")
+                break
+            elif(theBoard['1']==theBoard['2']==theBoard['3']!=' '):
+                printBoard(theBoard)
+                print("\nGame Over.\n")
+                print("****"+t+"won.****")
+                break
+            elif(theBoard['1']==theBoard['4']==theBoard['7']!=' '):
+                printBoard(theBoard)
+                print("\nGame Over.\n")
+                print("****"+t+"won.****")
+                break
+            elif(theBoard['2']==theBoard['5']==theBoard['8']!=' '):
+                printBoard(theBoard)
+                print("\nGame Over.\n")
+                print("****"+t+"won.****")
+                break
+            elif(theBoard['3']==theBoard['6']==theBoard['9']!=' '):
+                printBoard(theBoard)
+                print("\nGame Over.\n")
+                print("****"+t+"won.****")
+                break
+            elif(theBoard['1']==theBoard['5']==theBoard['9']!=' '):
+                printBoard(theBoard)
+                print("\nGame Over.\n")
+                print("****"+t+"won.****")
+                break
+            elif(theBoard['7']==theBoard['5']==theBoard['3']!=' '):
+                printBoard(theBoard)
+                print("\nGame Over.\n")
+                print("****"+t+"won.****")
+                break
+        if(c==9):
+            printBoard(theBoard)
+            print("\nGame Over.\n")
+            print("It's a Tie")
+        
+        if(t=='X'):
+            t='0'
+        else:
+            t= 'X'
+    restart = input("Do you want to play Again?(y/n)")
+    if(restart =='y' or restart=='Y'):
+        for key in board_keys:
+            theBoard[key]=" "
+        game()
+
+if  __name__=='__main__':
+    game()
+    
+        
+        
